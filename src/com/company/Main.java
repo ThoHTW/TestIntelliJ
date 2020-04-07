36bee9fb82c9dfa341d5868ffff2555b50b6b69e
ThoHTW <only4cs@freenet.de>
2020-04-07 13:48:03 +0200

    Wie ändere ich den Namen

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello GitHub");
+        System.out.println("Hello GitHub, jetzt nur namen ändern");
     }
 }

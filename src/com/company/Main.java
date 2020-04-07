98a3c39d807945934343d4bcf9abad437eb6f5ee
ThoHTW <only4cs@freenet.de>
2020-04-07 13:59:30 +0200

    WWarum lässt sich der Commiter nicht ändern, woher haben sie überhaupt die email

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello GitHub, jetzt nur namen ändern");
+        System.out.println("OOF, was soll das");
+
     }
 }
